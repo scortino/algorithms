a9b866d4167448707a84af72bc39122b85f5ff92
scortino <stefanocortinovis17@gmail.com>
2021-03-12 19:00:48 +0100

    Expose function when accessing mod

--- src/sorting/mod.rs
+++ src/sorting/mod.rs
@@ -3,3 +3,9 @@ mod insertion_sort;
 mod merge_sort;
 mod quick_sort;
 mod selection_sort;
+
+pub use self::counting_sort::counting_sort;
+pub use self::insertion_sort::insertion_sort;
+pub use self::merge_sort::merge_sort;
+pub use self::quick_sort::quick_sort;
+pub use self::selection_sort::selection_sort;
