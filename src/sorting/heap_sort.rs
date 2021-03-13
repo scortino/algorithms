30bfd9be01716fcef5dec04ed2b4e9d8e01bc20a
scortino <stefanocortinovis17@gmail.com>
2021-03-13 11:55:35 +0100

    In-place implementation of heap_sort

--- src/sorting/heap_sort.rs
+++ src/sorting/heap_sort.rs
@@ -1,16 +1,36 @@
-// TODO: in-place version
+fn sink(arr: &mut [i32], i: usize, size: usize) {
+    let (l, r) = (2 * i + 1, 2 * i + 2); // root's children
+    let mut largest = i; // assign i here
+    if l < size && arr[l] > arr[i] {
+        largest = l;
+    }
+    if r < size && arr[r] > arr[largest] {
+        largest = r;
+    }
+    if largest != i {
+        arr.swap(i, largest);
+        sink(arr, largest, size);
+    }
+}
 
-use crate::data_structures::MaxHeap;
+fn heapify(arr: &mut [i32]) {
+    let len = arr.len();
+    for i in (0..(len / 2 + 1)).rev() {
+        sink(arr, i, len);
+    }
+}
 
 #[allow(dead_code)]
-pub fn heap_sort(arr: Vec<i32>) -> Vec<i32> {
-    let mut heapified_arr = MaxHeap::from_vector(arr);
-    for i in (1..heapified_arr.len()).rev() {
-        heapified_arr.items.swap(0, i);
-        heapified_arr.size -= 1;
-        heapified_arr.max_heapify(0);
+pub fn heap_sort(arr: &mut [i32]) {
+    heapify(arr);
+    let len = arr.len();
+    let mut size = len;
+    for i in (1..len).rev() {
+        println!("{:?}", arr);
+        arr.swap(0, i);
+        size -= 1;
+        sink(arr, 0, size);
     }
-    heapified_arr.items
 }
 
 #[cfg(test)]
@@ -18,43 +38,50 @@ mod tests {
     use super::*;
     #[test]
     fn empty() {
-        let arr = vec![];
-        assert_eq!(heap_sort(arr), vec![]);
+        let mut arr = [];
+        heap_sort(&mut arr);
+        assert_eq!(arr, []);
     }
 
     #[test]
     fn one_element() {
-        let arr = vec![1];
-        assert_eq!(heap_sort(arr), vec![1]);
+        let mut arr = [1];
+        heap_sort(&mut arr);
+        assert_eq!(arr, [1]);
     }
 
     #[test]
     fn already_sorted() {
-        let arr = vec![1, 2, 3];
-        assert_eq!(heap_sort(arr), vec![1, 2, 3]);
+        let mut arr = [1, 2, 3];
+        heap_sort(&mut arr);
+        assert_eq!(arr, [1, 2, 3]);
     }
 
     #[test]
     fn even_number_of_elements() {
-        let arr = vec![1, 3, 5, 4];
-        assert_eq!(heap_sort(arr), vec![1, 3, 4, 5]);
+        let mut arr = [1, 3, 5, 4];
+        heap_sort(&mut arr);
+        assert_eq!(arr, [1, 3, 4, 5]);
     }
 
     #[test]
     fn odd_number_of_elements() {
-        let arr = vec![1, 42, 3, 5, 2];
-        assert_eq!(heap_sort(arr), vec![1, 2, 3, 5, 42]);
+        let mut arr = [1, 42, 3, 5, 2];
+        heap_sort(&mut arr);
+        assert_eq!(arr, [1, 2, 3, 5, 42]);
     }
 
     #[test]
     fn repeated_elements() {
-        let arr = vec![42, 42, 42, 42];
-        assert_eq!(heap_sort(arr), vec![42, 42, 42, 42]);
+        let mut arr = [42, 42, 42, 42];
+        heap_sort(&mut arr);
+        assert_eq!(arr, [42, 42, 42, 42]);
     }
 
     #[test]
     fn longer() {
-        let arr = vec![10, 8, 4, 3, 1, 9, 2, 7, 5, 6];
-        assert_eq!(heap_sort(arr), vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+        let mut arr = [10, 8, 4, 3, 1, 9, 2, 7, 5, 6];
+        heap_sort(&mut arr);
+        assert_eq!(arr, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
     }
 }
